ebfe0492eee8a134f4a503ded5b103968d97e065
Mahmoud salah eldien saber <31797198+mahmoudsalah37@users.noreply.github.com>
2021-10-09 13:05:24 +0200

    update

--- test/swipe_button_widget_test.dart
+++ test/swipe_button_widget_test.dart
@@ -1,5 +1,23 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:swipe_button_widget/swipe_button_widget.dart';
 
-void main() {
-  test('adds one to input values', () {});
-}
+// void main() {
+//   const MethodChannel channel = MethodChannel('swipe_button_widget');
+
+//   TestWidgetsFlutterBinding.ensureInitialized();
+
+//   setUp(() {
+//     channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//       return '42';
+//     });
+//   });
+
+//   tearDown(() {
+//     channel.setMockMethodCallHandler(null);
+//   });
+
+//   test('getPlatformVersion', () async {
+//     expect(await SwipeButtonWidget.platformVersion, '42');
+//   });
+// }
